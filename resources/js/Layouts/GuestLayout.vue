c8a5bcbc91c3cd38c4d23d856deb8b5064f1ab18
Andries De Baere <91671938+Andries-DB@users.noreply.github.com>
2025-06-11 12:48:01 +0200

    ✨Setup

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div
+        class="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0"
+    >
+        <div>
+            <Link href="/">
+                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+            </Link>
+        </div>
+
+        <div
+            class="mt-6 w-full overflow-hidden px-6 py-4 sm:max-w-md sm:rounded-lg flex min-h-full flex-col justify-center px-6 py-12 lg:px-8"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
