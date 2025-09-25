f35dbadb7ac7e3219407af5ae5b80df0b6d5fbf1
Andries De Baere <91671938+Andries-DB@users.noreply.github.com>
2025-09-25 16:08:46 +0200

    Update PrimaryButton.vue

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-secondary rounded hover:bg-blue-700 focus:outline-none"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none"
     >
         <slot />
     </button>
